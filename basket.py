0822875a58649b0cb05475e4e07c2afddccd382b
anthonyfadero <aofadero@gmail.com>
2014-07-01 21:38:33 +0100

    updated files

--- basket.py
+++ basket.py
@@ -1,1 +1,86 @@
+# coding=utf-8
 # program that adds products to a shopping basket and displays total
+
+class Product(object):
+
+    def __init__(self, name, description, price):
+        self.name = name
+        self.description = description
+        self.price = price
+
+    # method to display details of product
+    def display_product_details(self):
+        return "Product name: {0}, Desc: {1}, Price: £{2:.2f}".format(self.name, self.description, self.price)
+
+
+class Basket(object):
+
+    def __init__(self):
+        self.basket_items = []
+        self.basket = {}
+
+    # method to add product to basket items list
+    def add_product(self, product):
+        self.basket_items.append(product.name)
+        self.basket_items.append(product.description)
+        self.basket_items.append(product.price)
+
+        return self.basket_items
+
+    def add_prods(self, prod, quantity=1):
+        '''
+            This function adds a product to the basket which is a dictionary.
+            The default quantity is set to 1
+        '''
+        # set the first item in basket to have a key = 1
+        basket_key = 1
+        # increment basket_keys if already in self.basket
+        while basket_key in self.basket.keys():
+            basket_key += 1
+        # set value of key 1 in basket to a list of the product name, description, and price
+        self.basket[basket_key] = [prod.name, prod.description, prod.price, quantity]
+
+        return self.basket
+
+    def remove_item(self, product):
+        '''
+            This function removes an item from the basket
+        '''
+        # Iterate through items in basket and delete item that matches product.name
+        for key, product_name in self.basket.items():
+            if product_name[0] == product.name:
+                del self.basket[key]
+            else:
+                continue
+
+        return self.basket
+
+
+    def update_item(self, product):
+        '''
+            This function updates the quantity field of a product in the basket
+        '''
+        pass
+
+
+def main():
+
+    product1 = Product("Jam", "Marmalade", 2.45)
+    product2 = Product("Lurpak", "Butter", 5.00)
+    product3 = Product("Ariel", "Soap", 3.63)
+    basket1 = Basket()
+    basket1.add_prods(product1)
+    basket1.add_prods(product2)
+    basket1.add_prods(product3, 5)
+
+    print "Before ", basket1.basket
+
+    basket1.remove_item(product2)
+
+    print "After ", basket1.basket
+
+
+if __name__ == "__main__":
+    main()
+
+
--- basket_tests.py
+++ basket_tests.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # unit tests for basket program
 import unittest
 
@@ -9,7 +10,41 @@ class Basket_tests(unittest.TestCase):
 
     # define setUp for tests
     def setUp(self):
-        self.product = Product()
+        self.product = Product("Lego1", "Lego Toy", 2.50)
         self.basket = Basket()
 
-    
+    # create test for instance of Product class
+    def product_instance_test(self):
+        # create instance of product with name, description, and price attribute
+        self.product = Product("test", "This is a sample", 2.50)
+        # verify attributes of instance of product
+        self.assertEqual("test", self.product.name)
+        self.assertEqual("This is a sample", self.product.description)
+        self.assertEqual(2.50, self.product.price)
+
+    # create test for instance of Basket class
+    def basket_instance_test(self):
+        # create instance of basket class
+        self.basket = Basket()
+
+    # create test to display product details
+    def display_product_details_test(self):
+        result = self.product.display_product_details()
+        self.assertEqual("Product name: Lego1, Desc: Lego Toy, Price: £2.50", result)
+
+    # create test to add a product to the basket
+    def add_product_to_basket_test(self):
+        result = self.basket.add_product(self.product)
+        self.assertEqual(["Lego1", "Lego Toy", 2.50], result)
+
+    # create a test to add product using a dictionary as the container
+    def add_prod_to_basket_test(self):
+        result = self.basket.add_prods(self.product, 5)
+        self.assertEqual({1:["Lego1", "Lego Toy", 2.50, 5]}, result)
+
+    # create test to remove product item from basket items
+    def remove_item_from_basket_test(self):
+        # add product to basket first
+        self.basket.add_prods(self.product)
+        result = self.basket.remove_item(self.product)
+        self.assertEqual({}, result)
